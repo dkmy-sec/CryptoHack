6383b17c7af29ac649cf885d2d9e84f03c58c305
Kyle Lewis <klewdesign@gmail.com>
2025-08-21 22:11:33 -0400

    My CryptoHack Solutions; Modular Arithmetic

---
+++ modular_arithmetic_1.py
@@ -0,0 +1,11 @@
+# Example 1: 11 ≡ x mod 6
+x = 11 % 6
+print("x =", x)
+
+# Example 2: 8146798528947 ≡ y mod 17
+y = 8146798528947 % 17
+print("y =", y)
+
+# Get the smaller of the two
+result = min(x, y)
+print("Smaller value is:", result)
